a37794743578d8aa21d7f7fe1f00167faf0fa4a4
BMFowler <W0278875@nscc.ca>
2016-02-12 21:06:16 -0400

    Barebones Frame - pseudo coded, Revision 0.1b
    php files:
    Registration Page
    Home page
    Header partial
    login partial
    
     Java script files
    Registration
    
    css files:
    Header css

---
+++ Larva/resources/assets/Javascript/Registration.js
@@ -0,0 +1,11 @@
+function ValidateEmail(element){
+    //code to validate proper email format
+}
+
+function Validatepassword(element){
+    //code to validate proper password format
+}
+
+function ValidateUser(element){
+    //code to validate unique username
+}
---
+++ Larva/resources/views/Host.blade.php
@@ -0,0 +1,4 @@
+@extends('Header.Header')
+
+
+
