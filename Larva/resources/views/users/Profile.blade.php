66f86a22059a1a3dfee1de929e70b0e298184c20
domcull3n <w0278875@nscc.ca>
2016-03-30 06:14:48 -0700

    updated profile master page

--- Larva/resources/views/users/Profile.blade.php
+++ Larva/resources/views/users/Profile.blade.php
@@ -1,58 +1,11 @@
 @include('app')
 
-<div>
-    <?php
-    //if user is logged on show logout button
-    if(!isset($_SESSION["login"])){
-        echo  "Please login to view this page";
-    }
-    ?>
-</div>
-<html>
 <head>
-    <title>User Profile</title>
-
-    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            color: #B0BEC5;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-            margin-bottom: 40px;
-        }
-
-        .quote {
-            font-size: 24px;
-        }
-    </style>
+    <title>{{ $user->username }}'s Profile</title>
 </head>
 <body>
-<div class="container">
-    <div class="content">
-        <div class="title">User Profile</div>
+
+        <div class="title">{{ $user->username }}'s Profile</div>
         <div class="title">User Information</div>
-    </div>
-</div>
+
 </body>
-</html>
